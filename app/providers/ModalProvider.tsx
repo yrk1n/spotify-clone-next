21b1151dd6d197f780f56a0c2feed961c2081eae
Yrkn <118745292+yrk1n@users.noreply.github.com>
2024-06-24 18:51:37 +0300

    upload modal and functionality

--- app/providers/ModalProvider.tsx
+++ app/providers/ModalProvider.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import AuthModal from "@/components/AuthModal";
+import UploadModal from "@/components/Uploadmodal";
 import { useEffect, useState } from "react";
 
 const ModalProvider = () => {
@@ -15,6 +16,7 @@ const ModalProvider = () => {
   return (
     <>
       <AuthModal />
+      <UploadModal />
     </>
   );
 };
